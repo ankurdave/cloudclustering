6165f2d5cbabca42d85fa49643716213bbfc7cc2
t-ankurd@ANKURDAVE.redmond.corp.microsoft.com <none@none>
2010-07-06 11:04:31 -0700

    Renamed AzureHelper project/namespace to AzureUtils to avoid conflict with a class inside it
    --HG--
    rename : AzureHelper/AzureHelper.csproj => AzureHelper/AzureUtils.csproj

--- AzureHelper/AzureHelper.cs
+++ AzureHelper/AzureHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AzureHelper
+namespace AzureUtils
 {
     static class AzureHelper
     {
--- AzureHelper/ServerRequest.cs
+++ AzureHelper/ServerRequest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AzureHelper
+namespace AzureUtils
 {
     public class ServerRequest : IAzureMessage
     {
