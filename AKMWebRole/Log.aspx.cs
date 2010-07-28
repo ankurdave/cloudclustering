f97ea7baa23c4f0968cd86acc49b655e26d42066
Ankur Dave <t-ankurd@microsoft.com>
2010-07-28 10:29:05 -0700

    Started fixing format to match Wei's spec.

--- AKMWebRole/Log.aspx.cs
+++ AKMWebRole/Log.aspx.cs
@@ -21,13 +21,16 @@ namespace AKMWebRole
                 return;
             }
 
+            Response.ContentType = "text/plain";
+
             var logs = (AzureHelper.PerformanceLogger.PerformanceLogs
                 .Where(log => log.PartitionKey == jobID.ToString()) as DataServiceQuery<PerformanceLog>)
                 .Execute()
+                .Where(log => log.MethodName == "ProcessNewTask")
                 .OrderBy(log => log.StartTime)
-                .Select(log => string.Format("{0}\t{2}\tExecuting the task {3}{4}...\n"
-                    + "{1}\t{2}\tExecution of task {3}{4} is done, it takes {5} mins\n",
-                    log.StartTime, log.EndTime, log.PartitionKey, log.MethodName, log.IterationCount, (log.EndTime - log.StartTime).TotalMinutes));
+                .Select(log => string.Format("{0}\t{2}\tExecuting the task {3}{4}{5}...\n"
+                    + "{1}\t{2}\tExecution of task {3}{4}{5} is done, it takes {6} mins\n",
+                    log.StartTime.ToString("M/d/yyyy H:m"), log.EndTime.ToString("M/d/yyyy H:m"), log.PartitionKey, log.MethodName, log.IterationCount, log.RowKey, (log.EndTime - log.StartTime).TotalMinutes));
             Response.Write(string.Join("", logs));
         }
     }
