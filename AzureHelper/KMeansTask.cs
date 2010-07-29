33dee524b29ee8ece8ff739df70c5927dac3073d
Ankur Dave <t-ankurd@microsoft.com>
2010-07-29 12:15:10 -0700

    Added unknown files

---
+++ AzureHelper/KMeansTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AzureUtils
+{
+    public class KMeansTask
+    {
+        public KMeansTaskData TaskData { get; set; }
+        public bool Running { get; set; }
+
+        public KMeansTask(KMeansTaskData taskData, bool running = true)
+        {
+            this.TaskData = taskData;
+            this.Running = running;
+        }
+    }
+}
